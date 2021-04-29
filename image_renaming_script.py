b5f5ed802ef36c8fa8ae3d7b2e3f1f0111cd3327
fernando <okengfernando@github.com>
2021-04-29 16:08:18 +0300

    added error handling

--- image_renaming_script.py
+++ image_renaming_script.py
@@ -5,22 +5,38 @@
 # Into the Desired  
 
 import os
-  
-os.chdir('/home/fernando/raw_script_processor/collated/collated_IR_images')   #insert path to your raw images
-print(os.getcwd())      #displays current working directory
-COUNT = 1
-  
-# Function to increment count 
-# to make the files sorted.
-def increment():
-    global COUNT
-    COUNT = COUNT + 1
-  
+import sys
 
-for f in os.listdir():
-    f_name, f_ext = os.path.splitext(f) 
-    f_name = "RFI007_PAR09_ Vertical, 1m GSD_Wildfire_EO-IR_0" + str(COUNT)  #insert desired output format eg RFI006_PAR08_Vertical, 1m GSD_SAR-Aviation_0
-    increment()
+def main():
+
+    FOLDER_PATH = '/home/fernando/raw_script_processor/collated/collated_IR_images'  #insert path to your raw images
+    FILE_NAME = "RFI007_PAR09_ Vertical,1m GSD_Wildfire_EO-IR_0"   #insert desired output format NB: avoid / because it throws a FILE-NOT-FOUND Error
+
+    for i in FILE_NAME:
+        if "/" == i:
+            sys.exit("Error !!, remove foward or Back slash from FILE_NAME")
+
+    os.chdir(FOLDER_PATH)   
+    print(os.getcwd())      #displays current working directory
+    global COUNT 
+    COUNT = 1
     
-    new_name = '{} {}'.format(f_name, f_ext)
-    os.rename(f, new_name)
+    # Function to increment count 
+    # to make the files sorted.
+    def increment():
+        global COUNT
+        COUNT = COUNT + 1
+    
+
+    for f in os.listdir():
+        f_name, f_ext = os.path.splitext(f) 
+        f_name = FILE_NAME + str(COUNT)  
+        increment()
+        
+        new_name = '{} {}'.format(f_name, f_ext)
+        os.rename(f, new_name)
+        
+    print("successfully renamed {} files".format(len(os.listdir())))
+
+if __name__ == "__main__":
+    main()
