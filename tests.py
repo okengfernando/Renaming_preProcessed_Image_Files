3a0f6d2ad53ece036d6a38c1f42c815a4b8f1899
fernando <okengfernando@github.com>
2021-04-29 16:07:18 +0300

    added Test Cases

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+import image_renaming_script
+
+class TestFunctions(unittest.TestCase):
+    def test_case(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
