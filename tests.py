8045a6e36faa7c7c8e5ce280997412763c8c055a
fernando <okengfernando@github.com>
2021-07-09 15:41:21 +0300

    comprehensive testing added

--- tests.py
+++ tests.py
@@ -2,7 +2,16 @@ import unittest
 import image_renaming_script
 
 class TestFunctions(unittest.TestCase):
-    def test_case(self):
+    def test_case_lenght(self):
+        pass
+
+    def test_case_width(self):
+        pass
+
+    def test_case_pixel(self):
+        pass
+
+    def test_case_GSD(self):
         pass
 
 
