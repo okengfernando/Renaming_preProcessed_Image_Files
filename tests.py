822539d90c5e9cc146879ff8ee5c79915076d00d
fernando <okengfernando@github.com>
2021-07-12 15:33:19 +0300

    added more test cases

--- tests.py
+++ tests.py
@@ -13,6 +13,16 @@ class TestFunctions(unittest.TestCase):
 
     def test_case_GSD(self):
         pass
+    
+    def test_olbique(self):
+        pass
+
+    def test_vertical(self):
+        pass
+
+    def test_far(self):
+        pass
+    
 
 
 if __name__ == "__main__":
