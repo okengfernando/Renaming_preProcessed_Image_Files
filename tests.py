2c2f31cbd417dc3e1ce167e77fb5bca448cee7b9
fernando <okengfernando@github.com>
2021-07-19 17:50:49 +0300

    added fixes

--- tests.py
+++ tests.py
@@ -10,20 +10,9 @@ class TestFunctions(unittest.TestCase):
 
     def test_case_pixel(self):
         pass
-
-    def test_case_GSD(self):
-        pass
-    
-    def test_olbique(self):
-        pass
-
-    def test_vertical(self):
-        pass
-
-    def test_far(self):
-        pass
-    
+        
 
 
 if __name__ == "__main__":
     unittest.main()
+
