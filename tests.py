0693a30b40cc8775935135a0daf3c9d70d38ab39
fernando <okengfernando@github.com>
2021-07-20 19:14:53 +0300

    added new testcases

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 import image_renaming_script
 
 class TestFunctions(unittest.TestCase):
-    def test_case_lenght(self):
+    def test_case_length(self):
         pass
 
     def test_case_width(self):
@@ -10,6 +10,10 @@ class TestFunctions(unittest.TestCase):
 
     def test_case_pixel(self):
         pass
+
+    def test_case_points(self):
+        pass
+
         
 
 
