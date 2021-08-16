e28ae69086ede2539d6b14e68eb1c2c06dd7cd90
fernando <okengfernando@github.com>
2021-08-16 20:08:53 +0300

    removed redundant testCases

--- tests.py
+++ tests.py
@@ -10,23 +10,7 @@ class TestFunctions(unittest.TestCase):
 
     def test_case_pixel(self):
         pass
-    
-    def test_image_array_size(self):
-        pass
-
-    def test_image_sharpness(self):
-        pass
-
-    def test_image_scale(self):
-        pass
-
-    def test_image_sigma(self):
-        pass
-
-    def test_image_augmentation(self):
-        pass
-        
-
+  
 
 if __name__ == "__main__":
     unittest.main()
