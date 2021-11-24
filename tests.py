bb03adaa7f181794bca930fba5c552df4fd04979
fernando <okengfernando@github.com>
2021-11-24 16:41:07 +0300

    redundant tests removed

--- tests.py
+++ tests.py
@@ -11,14 +11,6 @@ class TestFunctions(unittest.TestCase):
     def test_case_pixel(self):
         pass
 
-    def test_case_aspect_ratio(self):
-        pass
-
-    def test_case_sharpness(self):
-        pass
-
-    def test_hue_colour_scheme(self):
-        pass
   
 
 if __name__ == "__main__":
